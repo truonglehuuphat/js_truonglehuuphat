6fe8666991c02e5a19623636f800e8134a9c6cbb
PhatTruong <phattruongspkt@gmail.com>
2026-07-05 14:41:44 +0700

    update CartItem page, CardSummary

--- module2/final_project_ts/shop-app/src/components/cart/CardSummary.tsx
+++ module2/final_project_ts/shop-app/src/components/cart/CardSummary.tsx
@@ -1,11 +1,35 @@
-import { Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, Typography } from "@mui/material";
 
+import { Link } from "@mui/material";
 
-const CardSummary = () =>{
+const CardSummary = ({ totalPrice }: { totalPrice: number }) => {
     return (
-        <Typography>
-            CardSummary
-        </Typography>
+        <Card
+        sx={{
+            borderRadius:3,
+            borderColor:"divider",
+            border:"1px solid",
+            position: {md:"sticky"},
+            top: {md: 90}
+        }}
+        >
+            <CardContent>
+                <Typography variant="h6" sx={{fontWeight:700}}>
+                    Order Summary
+                </Typography>
+                <Box sx={{justifyContent:"space-between", display:"flex", mt:'3'}}>
+                    <Typography>
+                        Total: 
+                    </Typography>
+                    <Typography sx={{fontWeight:700}}>
+                        ${totalPrice?.toFixed(2)}
+                    </Typography>
+                </Box>
+                <Button component={Link} to="/checkout" fullWidth variant="contained" sx={{ mt: 3 }}>
+                    Proceed to Checkout
+                </Button>
+            </CardContent>
+        </Card>
     )
 }
 
--- module2/final_project_ts/shop-app/src/components/cart/CartItem.tsx
+++ module2/final_project_ts/shop-app/src/components/cart/CartItem.tsx
@@ -1,12 +1,68 @@
-import { Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, CardMedia, IconButton, Stack, Typography } from "@mui/material";
 
+import DeleteIcon from "@mui/icons-material/Delete";
 
-const CardItem = () =>{
+import type { CartItem as CartItemType } from "../../types/cart";
+import { useCart } from "../../context/CartProvider";
+
+const CartItem = ({ item }: { item: CartItemType }) => {
+    const { dispatch } = useCart();
+
     return (
-        <Typography>
-            CardItem
-        </Typography>
+        <Card
+            sx={{
+                border: "1px solid",
+                borderRadius: 3,
+                borderColor: "divider",
+                mt: 1.8,
+                display: "flex"
+            }}
+        >
+            <CardMedia component="img" image={item?.thumbnail} sx={{ width: { xs: 104, md: 140 }, objectFit: "contain", p: 1.5 }} />
+            <CardContent sx={{ flex: 1, display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
+                <Typography fontWeight={700}>
+                    {item?.title}
+                </Typography>
+                <Typography color="secondary" mt={1}>
+                    ${Number(item?.price).toLocaleString()}
+                </Typography>
+                <Stack direction="row" spacing={1} mt={1.5} sx={{alignItems:"center"}}  >
+                    <Button variant="outlined" size="small" sx={{ minWidth: 36 }}
+                        onClick={() =>
+                            dispatch({
+                                type: "DECREASE_QUANTITY",
+                                payload: item.id
+                            })
+                        }
+                    >
+                        -
+                    </Button>
+                    <Typography>
+                        {item?.quantity}
+                    </Typography>
+                    <Button variant="outlined" size="small" sx={{ minWidth: 36 }}
+                        onClick={() =>
+                            dispatch({
+                                type: "INCREASE_QUANTITY",
+                                payload: item.id
+                            })
+                        }
+                    >
+                        +
+                    </Button>
+                    <IconButton>
+                        <DeleteIcon />
+                    </IconButton>
+                    <Box sx={{ ml: "auto", display: "flex", alignItems: "center" }}>
+                        <Typography sx={{fontWeight:700}}>
+                            {Number(item?.quantity * item?.price).toFixed(2)}
+                        </Typography>
+                    </Box>
+
+                </Stack>
+            </CardContent>
+        </Card>
     )
 }
 
-export default CardItem;
+export default CartItem;
