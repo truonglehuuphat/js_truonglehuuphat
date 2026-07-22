9a739cdc3ecf9f115de0655fb3b61862c19b0df5
PhatTruong <phattruongspkt@gmail.com>
2026-07-22 09:37:10 +0700

    update module 3 final project

--- module3/finall_project/bai5/prisma/seed.ts
+++ module3/finall_project/bai5/prisma/seed.ts
@@ -1,7 +1,29 @@
-import { PrismaClient } from "../prisma/";
+import { Connection } from "pg";
+import { PrismaClient } from "../src/generated/prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+    adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL })
+})
 
-async function main(){
-    await prisma.categories
+async function main() {
+
+    await prisma.product.deleteMany();
+    await prisma.category.deleteMay();
+
+    const categories = await Promise.all([
+        prisma.category.create({ date: { name: "Điện thoại", slug: "phone" } }),
+        prisma.category.create({ date: { name: "Laptop", slug: "Laptop" } }),
+        prisma.category.create({ date: { name: "Máy tính bảng", slug: "tablet" } }),
+        prisma.category.create({ date: { name: "Âm thanh", slug: "audio" } }),
+        prisma.category.create({ date: { name: "Phụ kiện", slug: "accessory" } }),
+    ])
+
+    await prisma.product.createMany({
+        data: [
+            { title: "iphone 15 Pro", price: 22990000, thumbnail: "...", brand: "Apple", stock: 10, categoryId: categories[0].id },
+            { title: "Samsung Galaxy S24 Utral", price: 25290000, thumbnail: "...", brand: "Samsung", stock: 9, categoryId: categories[0].id },
+        ],
+        skipDuplicates: true, //tránh lỗi nếu chạy seed nhiều lần
+    })
 }
